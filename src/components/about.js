3860c4991f288d9ba0ef2c80699bd74c01da3f44
Bryce Leonard <bryceleonard@Bryces-MacBook-Pro.local>
2024-09-10 14:02:25 -0600

    aboutme updates

--- src/components/about.js
+++ src/components/about.js
@@ -1,58 +1,25 @@
 import Photos from './photos.js';
 
-const aboutStyle = {
-    color: "white",
-    background: "#141414",
-    width: "95%",
-    margin: "0 auto",
-    borderRadius: "0 0 10px 10px",
-    minHeight: "100vh",
-    marginBottom: "50px",
-}
-
-const h1 = {
-    fontSize: "58px",
-    textAlign:  "left",
-    margin: "0px",
-    marginBottom: "25px",
-    borderBottom: "1px solid white",
-    paddingLeft: "25px",
-}
-
-const p = {
-    width: "100",
-}
-const container = {
-    width: "45%",
-    display: "inline-block",
-    verticalAlign: "top",
-    textAlign: "left",
-    paddingRight: "100px",
-}
-
-const moveDown = {
-    paddingTop: "100px",
-}
 
 
 export default function About() { 
     return (
-        <div style={aboutStyle}>
+        <div className="aboutStyle">
 
-            <div style={moveDown}>
-                <h1 style={h1}>About Me</h1>
-                    <div style={container}>
-                        <p style={p}>
+            <div className="moveDown">
+                <h1 >About Me</h1>
+                    <div className="container">
+                        <p >
                                  a passionate product manager with over 14 years of experience in the tech industry, blending my expertise as both a product manager and software engineer. My journey began with a BBA from Belmont University, where I developed a strong foundation in business and technology.
                             </p>
-                            <p style={p}>
+                            <p >
                                 I’m fortunate to share my life with my wonderful wife, Lindsey, and our two energetic kids, Theo and Remi. When I’m not working, you’ll likely find me immersed in my favorite hobbies. I’m an avid photographer with a love for medium format film, with a focus on landscapes. 
                             </p>
-                            <p style={p}>
+                            <p >
                                 In addition to photography, I enjoy building analog guitar effects pedals, which has given me a deeper appreciation for electronics and engineering. I also find joy in cooking and golfing—two activities that offer a perfect blend of creativity and relaxation.
                             </p>
 
-                            <p style={p}>
+                            <p >
                                 I am pleased to present my portfolio and look forward to the opportunity to connect with like-minded professionals and enthusiasts.
                             </p>
                     </div>
